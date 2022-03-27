4de84f4d7706c3e17a7d58f056e77e1a57e3ae7f
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2022-03-27 17:47:10 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -168,7 +168,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Mani5GRockers',
             'mimeType': mime_type,
         }
         if parent_id is not None:
